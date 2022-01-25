c6ed3365d36488bea649cb2aa18fb78b5eb3ccf1
simonhodson <simon@2132.io>
2022-01-25 17:47:22 +0000

    stable: flow complete for testing

--- src/ts-structures/types.ts
+++ src/ts-structures/types.ts
@@ -1,3 +1,3 @@
-export type ActionTypes = 'discount' | 'editList' | 'checkout' | 'complete' | 'receipt';
+export type ActionTypes = 'discount' | 'editList' | 'checkout' | 'complete' | 'receipt' | 'focus' | 'cancel';
 
 export type TransactionState = 'pending' | 'sending' | 'printing' | 'completed';
